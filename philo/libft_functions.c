13467302f91217f9c02fae11be3cbd8089b3ff3f
Juan Antonio Domínguez <97060625+jdomingu98@users.noreply.github.com>
2023-02-23 14:32:20 +0100

    added libraries, structs and main function

--- philo/libft_functions.c
+++ philo/libft_functions.c
@@ -1,31 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosophers.c                                     :+:      :+:    :+:   */
+/*   libft_functions.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jdomingu <jdomingu@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/19 14:37:10 by jdomingu          #+#    #+#             */
-/*   Updated: 2023/02/23 14:31:28 by jdomingu         ###   ########.fr       */
+/*   Created: 2023/02/23 14:22:39 by jdomingu          #+#    #+#             */
+/*   Updated: 2023/02/23 14:23:07 by jdomingu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-static int	init_values(t_data *data, int argc, char **argv)
-{
-	
-}
 
-int main(int argc, char **argv)
-{
-	t_data	data;
-
-	if (argc < 5 || argc > 6)
-	{
-		write(1, "Error\n", 6);
-		return (-1);
-	}
-	init_values(&data, argc, argv);
-	return (0);
-}
--- philo/philosophers.h
+++ philo/philosophers.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jdomingu <jdomingu@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/20 20:27:36 by jdomingu          #+#    #+#             */
-/*   Updated: 2023/02/20 22:03:41 by jdomingu         ###   ########.fr       */
+/*   Created: 2023/02/19 14:37:58 by jdomingu          #+#    #+#             */
+/*   Updated: 2023/02/23 14:22:20 by jdomingu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,37 @@
 # define PHILOSOPHERS_H
 
 # include <unistd.h>
+# include <stdio.h>
+# include <string.h>
 # include <stdlib.h>
 # include <sys/time.h>
-# include <stdio.h>
 # include <pthread.h>
-# include <limits.h>
 
-typedef struct s_philo
+typedef enum e_state
 {
-	int	nose;
-} t_philo;
+	EATING,
+	SLEEPING,
+	THINKING
+} t_state;
 
+typedef struct s_philos
+{
+	int			philo;
+	int			last_eat;
+	t_state		state;
+	t_philos	*next_philo;
+} t_philos;
 
+typedef struct s_data
+{
+	int			nbr_philos;
+	int			die_time;
+	int			eat_time;
+	int			sleep_time;
+	int			num_eats;
+	t_philos	*philo;
+} t_data;
+
+void	init_values(t_data *data, int argc, char **argv);
 
 #endif
