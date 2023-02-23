c964d09ad2abc0e210f0972efd473e7bd5ce25ba
Juan Antonio Domínguez <97060625+jdomingu98@users.noreply.github.com>
2023-02-23 12:21:20 +0100

    added base structure

---
+++ philo/philosophers.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdomingu <jdomingu@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 20:27:36 by jdomingu          #+#    #+#             */
+/*   Updated: 2023/02/20 22:03:41 by jdomingu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <stdio.h>
+# include <pthread.h>
+# include <limits.h>
+
+typedef struct s_philo
+{
+	int	nose;
+} t_philo;
+
+
+
+#endif
